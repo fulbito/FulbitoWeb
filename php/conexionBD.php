9f24b8035a0e32beb7e0768369de030378fc0a9f
Fabian <fabianmayoral@hotmail.com>
2014-04-06 00:31:57 -0300

    Fucking BD

--- php/conexionBD.php
+++ php/conexionBD.php
@@ -1,8 +1,19 @@
 <?php
 	global $db;
+    /*
+    // DATOS DE LOCALHOST MAYO
     $usuario="root";
     $clave="root";
-    $nombreBD="fulbito";  
+    $nombreBD="fulbito";
     $conexion="localhost";
+    */
+
+    // DATOS DE LOCALHOST ADRI
+    $usuario="root";
+    $clave="palermo9";
+    $nombreBD="a6003835_fulbito";
+    $conexion="localhost";
+
 	$db = new ezSQL_mysql($usuario,$clave,$nombreBD,$conexion);
+
 ?>
